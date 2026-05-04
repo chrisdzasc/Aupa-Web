5268e287616d1603559303ca0e2393df2d579603
Christian Diaz <christian.dzasc@gmail.com>
2026-05-04 17:03:27 -0600

    feat: validaciones del formulario de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,9 +7,30 @@ function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errores, setErrores] = useState<{ email?: string; password?: string }>({});
+
+    const validar = () => {
+        const nuevoErrores: { email?: string, password?: string } = {};
+
+        if(!email.trim()) {
+            nuevoErrores.email = "El correo electrónico es obligatorio";
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            nuevoErrores.email = "Ingresa un correo electrónico válido";
+        }
+
+        if(!password.trim()) {
+            nuevoErrores.password = "La contraseña es obligatoria";
+        }
+
+        setErrores(nuevoErrores);
+
+        return Object.keys(nuevoErrores).length === 0;
+    };
 
     const handleLogin = () => {
-        navigate("/dashboard");
+        if(validar()) {
+            navigate("/dashboard");
+        }
     };
 
     return (
@@ -33,10 +55,16 @@ function Login() {
                         <input 
                             type="email"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                                if(errores.email) setErrores({...errores, email: undefined});
+                            }}
                             placeholder="tucorreo@gmail.com"
-                            className="w-full px-4 py-3 border border-teal-200 rounded-lg text-sm focus:outline-none focus:border-teal-500"
+                            className={`w-full px-4 py-3 border rounded-lg text-sm focus:outline-none ${ errores.email ? "border-red-400 focus:boder-red-500" : "border-teal-200 focus:border-teal-500" }`}
                         />
+                        {errores.email && (
+                            <p className="text-xs text-red-500 mt-1">{errores.email}</p>
+                        )}
                     </div>
 
                     <div className="mb-2">
@@ -45,10 +73,16 @@ function Login() {
                         <input 
                             type="password"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                                if(errores.password) setErrores({ ...errores, password: undefined });
+                            }}
                             placeholder="••••••••"
-                            className="w-full px-4 py-3 border border-teal-200 rounded-lg text-sm focus:outline-none focus:border-teal-500"
+                            className={`w-full px-4 py-3 border rounded-lg text-sm focus:outline-none ${ errores.password ? "border-red-400 focus:border-red-500" : "border-teal-200 focus:border-teal-500" }`}
                         />
+                        {errores.password && (
+                            <p className="text-xs text-red-500 mt-1">{errores.password}</p>
+                        )}
                     </div>
 
                     <div className="text-right mb-6">
